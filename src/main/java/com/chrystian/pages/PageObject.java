18e9b259553dc475a7d695e86a99d30c73154d0d
chrystian_rosa <chrystian_rosa@sicredi.com.br>
2020-07-06 00:07:18 -0300

    setting project up

---
+++ src/main/java/com/chrystian/pages/PageObject.java
@@ -0,0 +1,20 @@
+package com.chrystian.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class PageObject {
+
+    protected WebDriver driver;
+    protected WebDriverWait waiter;
+
+    public PageObject(WebDriver pageDriver) {
+        driver = pageDriver;
+        waiter = new WebDriverWait(driver, 15);
+        waiter.withTimeout(Duration.ofSeconds(15)).pollingEvery(Duration.ofSeconds(1));
+        PageFactory.initElements(driver, this);
+    }
+}
